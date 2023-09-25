f82139d9153128f9fb40aeab24bec4a734348545
goodnews <goodnewsj62@gmail.com>
2023-09-25 03:39:03 +0100

    changes

--- theObserver/javaObservable/Main.java
+++ theObserver/javaObservable/Main.java
@@ -1,5 +1,5 @@
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         WeatherData weatherData =  new WeatherData();
         
         CurrentCondition currentCondition =  new CurrentCondition(weatherData);
@@ -12,6 +12,5 @@ public class Main{
         weatherData.setMeasurement(100.09, 1.1, 56.78);
 
         currentCondition.display();
-
     }
 }
